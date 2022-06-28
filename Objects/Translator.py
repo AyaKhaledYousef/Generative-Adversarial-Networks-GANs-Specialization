96cb5e880c2b22bc349264d59ef6607f935f46ff
AyaKhaledYousef <aya.k.yousef@gmail.com>
2022-06-28 14:53:11 +0200

    add

---
+++ Objects/Translator.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome
+from time import sleep
+
+
+class Translator:
+    def __init__(self, headless=False):
+        options = Options()
+        if headless: options.headless = True
+        self._br = Chrome(chrome_options=options)
+        self._br.get("https://www.freetranslations.org/english-to-arabic-translation.html")
+        # self._br.set_page_load_timeout(25)
+
+    def translate(self, string):
+        if not string: return "Done"
+        for i in range(3):
+            try: return self._translate(string.replace("\\", ''))
+            except Exception as e:
+                print(e)
+                sleep(2)
+        raise ConnectionAbortedError
+
+    def _translate(self, string):
+        tag = self._br.find_element_by_id("InputText")
+        tag.send_keys(Keys.CONTROL + "a")
+        tag.send_keys(Keys.DELETE)
+        tag.send_keys(string)
+        self._br.find_element_by_class_name("translate-form-control").click()
+        WebDriverWait(self._br, 10).until(ec.presence_of_element_located((By.CLASS_NAME, "mttextarea")))
+        return self._br.find_element_by_xpath("//*[@id='TranslationOutput']/div").text
+
+    def close(self):
+        self._br.close()
--- Objects/detection.py
+++ Objects/detection.py
@@ -2,20 +2,38 @@ import torch
 import gtts
 from pydub import AudioSegment
 from pydub.playback import play
+import numpy as np
+import cv2
 
-# Model
+
+def cam():
+    cap = cv2.VideoCapture(0)
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        img_name = "Glasses2/Objects/img.png"
+        cv2.imshow('frame', frame)
+        k=cv2.waitKey(1)
+
+        if k%256 == 27:
+            break
+        elif k%256 == 32:
+            print("take image")
+            cv2.imwrite(img_name, frame)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-# Image
-im = 'yolov5/data/images/bus.jpg'
-
-# Inference
-results = model(im)
+results = model('Glasses2/Objects/img.png')
 res=results.pandas().xyxy[0]
 for object in range(res.shape[0]):
     object_name=res['name'].values[object]
     print(object_name)
+
     tts = gtts.gTTS(object_name)
-    tts.save("object.mp3")
-    play(AudioSegment.from_mp3("object.mp3"))
+    tts.save("Glasses2/Objects/object.mp3")
+    play(AudioSegment.from_mp3("Glasses2/Objects/object.mp3"))
 
 
