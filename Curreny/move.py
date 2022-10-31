da84c99b3d389162c3c125c7329bbf7ca441da2a
ayakhaledyousef <aya.k.yousef@gmail.com>
2022-10-31 19:46:42 +0200

    delete last project

--- Curreny/move.py
+++
@@ -1,7 +0,0 @@
-import os
-import glob
-import shutil
-
-for i in glob.glob('NEW/*/*'):
-    shutil.move(i,'images/')
-
--- Curreny/rename.py
+++
@@ -1,7 +0,0 @@
-import os
-import glob
-count=0
-for i in glob.glob('NEW/200 pounds/*.JPG'):
-    print(i)
-    count+=1
-    os.rename(i,'NEW/200 pounds/200-{}'.format(count))
